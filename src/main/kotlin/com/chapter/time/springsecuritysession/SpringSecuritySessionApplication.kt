310a88200a9efdcfdd03e09a5bc1c93da2c0346b
Han Lin <Han.Lin@ing.com>
2021-03-05 14:30:20 +0100

    feat: empty repo

---
+++ src/main/kotlin/com/chapter/time/springsecuritysession/SpringSecuritySessionApplication.kt
@@ -0,0 +1,11 @@
+package com.chapter.time.springsecuritysession
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringSecuritySessionApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringSecuritySessionApplication>(*args)
+}
---
+++ src/test/kotlin/com/chapter/time/springsecuritysession/SpringSecuritySessionApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.chapter.time.springsecuritysession
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringSecuritySessionApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

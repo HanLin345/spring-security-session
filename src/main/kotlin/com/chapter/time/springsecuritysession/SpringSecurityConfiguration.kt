86d35c8b9ab3a24d3146be3c3c4ef9cc179a2fdd
Deniz Silahcilar <deniz.silahcilar@ing.com>
2021-03-11 17:26:58 +0100

    jdbc authentication added

--- src/main/kotlin/com/chapter/time/springsecuritysession/SpringSecurityConfiguration.kt
+++ src/main/kotlin/com/chapter/time/springsecuritysession/SpringSecurityConfiguration.kt
@@ -1,17 +1,24 @@
 package com.chapter.time.springsecuritysession
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.crypto.password.NoOpPasswordEncoder
+import javax.sql.DataSource
 
 @EnableWebSecurity
 class SpringSecurityConfiguration : WebSecurityConfigurerAdapter() {
 
+    @Autowired
+    lateinit var dataSource: DataSource
+
     override fun configure(auth: AuthenticationManagerBuilder) {
-        auth.inMemoryAuthentication()
+        auth.jdbcAuthentication()
+            .dataSource(dataSource)
+            .withDefaultSchema()
             .withUser("deniz")
             .password("admin")
             .roles("ADMIN")
@@ -19,7 +26,6 @@ class SpringSecurityConfiguration : WebSecurityConfigurerAdapter() {
             .withUser("nikita")
             .password("123")
             .roles("USER")
-
     }
 
     override fun configure(http: HttpSecurity) {
